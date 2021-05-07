08b2a8b21823368f54dbf80477172d7a1a05dbde
KoredeDavid <koredeoluwashola@gmail.com>
2021-05-07 08:43:42 +0100

    Made Debug to be only True if it is set to true from the configuration

--- anonymous/settings/prod.py
+++ anonymous/settings/prod.py
@@ -3,7 +3,7 @@ from .base import *
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', False)
+DEBUG = os.environ.get('DEBUG', False) == 'True'
 
 ALLOWED_HOSTS = ['concealme.herokuapp.com', '127.0.0.1']
 
